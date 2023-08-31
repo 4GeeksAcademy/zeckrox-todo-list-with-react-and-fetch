becf2df327a826a6e3be43dcf3384444d0845c9c
Stefano <tefanoboschetti@gmail.com>
2023-08-31 03:42:38 +0000

    Now you can delete all Task & API user is created when no task are in the ToDo

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -9,6 +9,7 @@ const ToDo = () => {
 		fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox')
 		.then(function(response) {
 		   if (!response.ok) {
+			console.error("Insert task to create user on API")
 		   throw Error(response.statusText);
 		}
 		// Read the response as json.
@@ -20,7 +21,16 @@ const ToDo = () => {
 		}) , []);
 
 	const deleteTodo = (elm) => {
-		if(todo.length === 1){alert("Must leave at least one task")}
+		if(todo.length === 1){fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox', {
+			method: "DELETE"})
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(data =>{
+				setTodo([])
+			})
+		}
 		if(todo.length > 1){
 		fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox', {
 				method: "PUT",
@@ -35,10 +45,40 @@ const ToDo = () => {
 				})
 			}}
 
+			function deleteAll(){
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox', {
+				method: "DELETE"})
+				.then(res => {
+					if (!res.ok) throw Error(res.statusText);
+					return res.json();
+				})
+				.then(data =>{
+					setTodo([])
+				})}
+
 	function insertTodo (text){
 		if (text.key ==="Enter"){
-		
-			fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox', {
+			if(text.target.value===""){return}
+			if(todo.length===0){
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox', {
+					method: "POST",
+					body: JSON.stringify([]),
+					headers:{'Content-Type': 'application/json'}})
+					.then(()=>{fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox', {
+						method: "PUT",
+						body: JSON.stringify([{label: text.target.value, id:text.target.value + todo.length, done:false}]),
+						headers:{'Content-Type': 'application/json'}})
+						.then(res => {
+							if (!res.ok) throw Error(res.statusText);
+							return res.json();
+						})
+						.then(data =>{
+							setTodo([{label: text.target.value, id:text.target.value + todo.length, done:false}])
+							text.target.value=""
+						})})
+			}
+			
+			if(todo.length>0){fetch('https://playground.4geeks.com/apis/fake/todos/user/zeckrox', {
 				method: "PUT",
 				body: JSON.stringify([...todo, {label: text.target.value, id:text.target.value + todo.length, done:false}]),
 				headers:{'Content-Type': 'application/json'}})
@@ -49,11 +89,11 @@ const ToDo = () => {
 				.then(data =>{
 					setTodo([...todo, {label: text.target.value, id:text.target.value + todo.length, done:false}])
 					text.target.value=""
-				})
+				})}
 
 		}}
 
-	return <><h1 className="d-flex justify-content-center">To Do</h1>
+	return <><button onClick={()=>{deleteAll()}}>delete all</button><h1 className="d-flex justify-content-center">To Do</h1>
 	<div className="todo">
 	<input  placeholder="Add ToDo" onKeyDown={(e)=>{insertTodo(e)}}></input>
 	{todo.map((todo) => (<TodoElement key={todo.id} text={todo.label} click={()=>{deleteTodo(todo)}} />))}
